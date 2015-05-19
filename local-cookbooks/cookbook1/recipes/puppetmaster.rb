4491908382a2eeb2df60dea3ba3832b0166295f7
Chris Chapman <chris@blackcompanystudios.co.uk>
2015-05-19 17:20:13 +0100

    Added a configuration folder (imported from the puppet repo

--- local-cookbooks/cookbook1/recipes/puppetmaster.rb
+++ local-cookbooks/cookbook1/recipes/puppetmaster.rb
@@ -2,4 +2,11 @@ include_recipe "apt"
 
 package "puppetmaster-passenger"
 
+remote_directory "/etc/puppet" do
+  path "/etc/puppet"
+  source "puppet"
+  recursive true
+  action :create
+end
+
 Chef::Log.info("***Installed puppet master***")
