5ccad22e8fed645cfe2db24c1f7d987e415d029e
Chris Chapman <chris@blackcompanystudios.co.uk>
2015-05-25 15:42:30 +0100

    First pass on a recipe that downloads and installs git, and Puppt

--- cookbook2/recipes/puppetnode.rb
+++ cookbook2/recipes/puppetnode.rb
@@ -1,1 +1,28 @@
 Chef::Log.info("***About to install puppet node***")
+
+directory 'C:\tmp' do
+  rights :full_control, 'Everyone'
+  recursive true
+  action :create
+end
+
+cookbook_file 'C:\tmp\git-1.9.5-preview20150319.exe' do
+  source "git-1.9.5-preview20150319.exe"
+  rights :full_control, 'Everyone'
+  action :create
+end
+
+windows_package 'Git' do
+  source 'c:\tmp\git-1.9.5-preview20150319.exe'
+  action :install
+end
+
+powershell_script 'Download Puppet Installer' do
+  code 'Invoke-WebRequest https://downloads.puppetlabs.com/windows/puppet-3.7.5-x64.msi -OutFile c:\tmp\puppet-3.7.5-x64.msi'
+  not_if 'Test-Path c:\tmp\puppet-3.7.5-x64.msi'
+end
+
+windows_package 'Puppet' do
+  source 'c:\tmp\puppet-3.7.5-x64.msi'
+  action :install
+end
